2ca887b4f94060b39d58b4f53c786c99802eca6b
James Cor <jcor@ucsd.edu>
2020-08-11 21:25:48 -0700

    working as before

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require('express');
+const socketIO = require('socket.io');
 var app = express();
-app.use(express.static('public'));
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+//var http = require('http').createServer(app);
+//var io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3000;
+const INDEX = '/public/index.html';
+
+// allow multiple files to be public
+app.use(express.static('public')); 
+
+const server = app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+                  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// Create a Socket.io server
+const io = socketIO(server);
+
+// Handle connections
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+// Broadcast updates
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+// Display static HTML page (hopefully including JS)
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Listen for chat message being emitted
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       io.emit('chat message', msg);
     });
   });
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
